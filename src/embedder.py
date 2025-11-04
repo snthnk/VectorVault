bf65d7f0249f734437c7718e433059776e86bc4a
snthnk <senya.novozhilov@gmail.com>
2025-11-04 19:21:09 +0300

    fixed logic, added logging, added tests

--- src/embedder.py
+++ src/embedder.py
@@ -1,17 +1,47 @@
 from transformers import AutoTokenizer, AutoModel
 import numpy as np
 from typing import *
+import logging
+import torch
 
-tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
-model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+logging.basicConfig(level=logging.DEBUG)
 
 class EmbeddingModel():
-    def encode(self, texts: List[str]) -> np.ndarray:
-        inputs = tokenizer(texts, return_tensors="pt")
-        outputs = model(**inputs)
-        embeddings = outputs.last_hidden_state[:, 0, :]
-        embeddings_np = embeddings.detach().cpu().numpy()
+    def __init__(self, device: str = "cpu"):
+        try:
+            self.tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+            logging.info("Токенизатор успешно загружен")
+        except:
+            logging.error("Не удалось загрузить токенизатор")
+        try:
+            self.model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+            logging.info("Модель успешно загружена")
+        except:
+            logging.error("Не удалось загрузить модель")
+        device = torch.device(device)
+        self.model.to(device)
+        logging.info(f"Текущий девайс: {device}")
+
+    def encode(self, texts: List[str], batch_size: int) -> np.ndarray:
+        embeddings_np = np.array([])
+        logging.info("Генерация эмбеддингов...")
+        if batch_size >= len(texts):
+            inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+            outputs = self.model(**inputs)
+            embeddings = outputs.last_hidden_state[:, 0, :]
+            embeddings_np = embeddings.detach().cpu().numpy()
+        else:
+            for i in range(0, len(texts), batch_size):
+                logging.info(f"Батч {i+1}/{len(texts)/batch_size}")
+                batch = texts[i*batch_size:i*batch_size+batch_size]
+                inputs = self.tokenizer(batch, padding=True, truncation=True, return_tensors="pt")
+                outputs = self.model(**inputs)
+                embeddings = outputs.last_hidden_state[:, 0: :]
+                embeddings_np = embeddings.detach().cpu().numpy()
         return embeddings_np
+
+    def get_embeddings_dim(self):
+        return self.model.config.hidden_size
 
 
 
--- src/vector_store.py
+++ src/vector_store.py
@@ -1,32 +1,58 @@
 import faiss
 import numpy as np
 from typing import List, Optional, Dict
+import random
+from datetime import datetime
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 class VectorStore():
     def __init__(self, embeddings_dim: int, use_cosine_similarity: bool):
+        logging.info("Инициализация векторного хранилища...")
         if use_cosine_similarity:
             self.index = faiss.IndexFlatIP(embeddings_dim)
         else:
             self.index = faiss.IndexFlatL2(embeddings_dim)
-        self.metadata = {}
+        self.metadata = []
         self.use_cosine_similarity = use_cosine_similarity
 
-    def add_vectors(self, vectors: np.ndarray, texts: List[str], doc_ids: Optional[List[str]]=None) -> List[Dict]:
+    def __len__(self):
+        return len(self.metadata)
+
+    def doc(self, id: int) -> Dict:
+        logging.info("Поиск документа по индексу...")
+        for i in range(len(self.metadata)):
+            if self.metadata[i]["doc_id"] == id:
+                return self.metadata[i]
+        return {}
+
+    def add_vectors(self, vectors: np.ndarray, texts: List[str], doc_ids: Optional[List[str]]=None) -> int:
+        logging.info("Добавление векторов...")
+        vectors = np.ascontiguousarray(vectors, dtype=np.float32)
         if self.use_cosine_similarity:
-            normalized = faiss.normalize_L2(vectors)
-            self.index.add(normalized)
+            faiss.normalize_L2(vectors)
+        index_start = self.index.ntotal
+        self.index.add(vectors)
+        if not doc_ids:
+            for i in range(len(texts)):
+                self.metadata.append({"doc_id": random.randint(1, 100000), "text": texts[i], "index_position": i+index_start, "timestamp": datetime.now().isoformat()})
         else:
-            self.index.add(vectors)
-        self.metadata = {doc_ids[i]: texts[i] for i in range(len(texts))}
+            for i in range(len(texts)):
+                self.metadata.append({"doc_id": doc_ids[i], "text": texts[i], "index_position": i+index_start, "timestamp": datetime.now().isoformat()})
         return vectors.shape[0]
 
-    def search(self, query_vector: np.ndarray, top_k: int = 5) -> List[Dict]:
+    def search(self, query_vector: np.ndarray, top_k: int = 5, threshold: float = 0.5) -> List[Dict]:
+        logging.info("Поиск в пространстве векторов...")
+        if self.index.ntotal == 0:
+            return []
         if self.use_cosine_similarity:
-            normalized = faiss.normalize_L2(query_vector)
+            faiss.normalize_L2(query_vector)
         distances, indices = self.index.search(query_vector, top_k)
         res = []
-        for idx in indices:
-            res.append(self.metadata[idx])
+        for i, idx in enumerate(indices[0]):
+            if distances[0][i] >= threshold:
+                res.append({"metadata": self.metadata[idx], "distance": distances[0][i]})
         return res
 
 
