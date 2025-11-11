cbf9f54f478d163d80be92c453b93a7ff70e9320
arseniy <senya.novozhilov@gmail.com>
2025-11-11 21:52:17 +0300

    added docker

--- src/main.py
+++ src/main.py
@@ -100,6 +100,6 @@ def clear():
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
 
 
